a456f8167a447a6edf7ab25c3ad031ab8120cf17
MAL <kinmal1988@gmail.com>
2023-02-16 17:29:12 +0300

    Added HomeWork_2

---
+++ HomeWork_2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 10.
+// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// Console.Write("Input a three-digit number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if ((99 < num && num < 1000) || (-1000 < num && num < -99))
+// {
+//     int CutNumber(int num)
+//      {
+//         int result = num/ 10 % 10;
+//         return result;
+//      }
+//      int newNumber = CutNumber(num);
+
+//     Console.WriteLine($"Your twice digit {newNumber} of three-digit number {num}");
+// }
+// else
+// Console.WriteLine($"Your number {num} not three-digit number");
+
+
+// Задача 13. 
+// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int temp = num;
+// if (num >= 100 || num <= -100)
+// {   
+//     while (temp >= 1000 || temp <= -1000 )
+//             temp = temp / 10;
+//     int result = temp % 10;
+
+//     Console.WriteLine($"the third digit of {num} is {result}");
+// }
+// else
+// Console.WriteLine($"the number {num} does not consist of three digits");
+
+
+// Задача 15.
+// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num < 8)
+// {
+//     if (num < 6)
+//     Console.WriteLine($"the day {num} is worker");
+// else
+// Console.WriteLine($"the day {num} is weekend");
+// }
+// else 
+// Console.WriteLine($"in a week of 7 days, You entered {num}");
---
+++ Seminar_2/Program.cs
@@ -0,0 +1,94 @@
+﻿// Задача 1.
+// Напишите программу, которая выводит случайное трёхзначное 
+// число и удаляет вторую цифру этого числа.
+// Пример из числа 345 - число 35
+
+// int CutNumber(int num) 
+// {
+//     int hundreds = num / 100;
+//     int units = num % 10;
+//     int resolt = hundreds * 10 + units;
+
+//     return resolt;
+// }
+
+// int randNumber = new Random().Next(100,1000);
+// int newNumber = CutNumber(randNumber);
+
+// Console.WriteLine($"New version of {randNumber} is {newNumber}");
+
+
+// Задача 2.
+// Напишите программу, которая принимает на вход число 
+// и проверяет, кратно ли оно одновременно a и b.
+// IsMultiplyed - кратное,  && - и
+
+// Вариант 1
+// bool IsMultiplyedBest(int num, int div1, int div2)
+// {
+//     return num % div1 == 0 && num % div2 == 0;
+// }
+
+// Вариант 2
+// bool IsMultiplyed(int num, int div1, int div2)
+// {
+//     if(num % div1 == 0 && num % div2 == 0)  // Или  if(num % div1*div2 ==0)         
+//         return true;
+//     else
+//         return false;
+// }
+
+// Console.Write("Input a number for checking: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a first divider: ");
+// int divider1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second divider: ");
+// int divider2 = Convert.ToInt32(Console.ReadLine());
+
+// bool result = IsMultiplyed(number, divider1, divider2);
+// if(result)
+
+//     Console.WriteLine($"Your number {number} is divisible by the {divider1} and {divider2}");
+// else 
+// Console.WriteLine($"Your number {number} is not divisible by the {divider1} and {divider2}");
+
+
+// Задача 3.
+// Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа
+
+// int maxNumber (int num)
+// {
+//     int tens = num / 10;
+//     int units = num % 10; 
+//     if (units > tens)
+//         return units;
+//     else
+//         return tens;
+// }
+
+// int randNumber = new Random().Next(10,100);
+// int max = maxNumber(randNumber);
+// Console.WriteLine($"The max sing of number {randNumber} is {max}");
+
+
+// Задача. 4
+// Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом другого.
+// ||- или
+
+// Console.Write("Input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// bool SquareOrNot(int num1, int num2)
+// {
+//     return (num1 / num2 ==num2|| num2 / num1 == num1);
+// }
+
+// bool resolt = SquareOrNot(num1, num2);
+// if(resolt)
+//     Console.WriteLine($"Number {num1} or number {num2} is square of the another number");
+// else
+// Console.WriteLine($"Number {num1} or number {num2} is not square of the another number");
